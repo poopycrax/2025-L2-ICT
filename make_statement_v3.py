06fbc2481fd96d3bb235a7e65b7b41d9cbaf1e2b
Andre Drew <andredrew@mmc.school.nz>
2025-05-06 09:11:47 +1200

    Flexible multi_line statement generator component

---
+++ make_statement_v3.py
@@ -0,0 +1,23 @@
+# Function
+def make_statement(statement, decoration, lines=1):
+    middle = f"{decoration * 3} {statement} {decoration * 3}"
+    top_bottom = decoration * len(middle)
+
+    if lines == 1:
+        print(middle)
+    elif lines == 2:
+        print(middle)
+        print(top_bottom)
+
+    else:
+        print(top_bottom)
+        print(middle)
+        print(top_bottom)
+
+
+# Main Routine
+make_statement("Programming is Fun!", "=", 3)
+print()
+make_statement("Programming is Still Fun!", "*", 2)
+print()
+make_statement("Emoji in Action", "👍")
